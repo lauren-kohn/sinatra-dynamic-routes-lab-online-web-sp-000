c872a831cea452e60b3d86a6984b3d9c760c4834
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-16 18:31:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,9 +21,9 @@ class App < Sinatra::Base
     "#{@repeats}"
   end 
   
-  #get '/say/:word1/:word2/:word3/:word4/:word5'
-  
-  #end
+  get '/say/:word1/:word2/:word3/:word4/:word5'
+    "#{params[:word1]} + #{params[:word2]} + #{params[:word3]} + #{params[:word4]} + #{params[:word5]}"
+  end
   
   #get '/:operation/:number1/:number2'
   #  .to_s
