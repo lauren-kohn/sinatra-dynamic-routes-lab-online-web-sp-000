9335e8ce7796b9b32e6085cc3f06911f0109f714
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-16 18:52:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,9 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @number1 = params[:number1]
     @number2 = params[:number2]
+    @operation = params[:operation]
+    if @operation = "add"
+      "#{}"
     .to_s
   end
 
