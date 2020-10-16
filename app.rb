d810cefff7c67f10b9ee12b35c11054af546ea88
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-16 18:21:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,8 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    
+    @repeats = @phrase.times do @number 
+    "#{@repeats}"
   end 
   
   #get '/say/:word1/:word2/:word3/:word4/:word5'
